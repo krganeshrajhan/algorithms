2e25a391c55606c6bd30d93661ee7cd134409ca7
ganeshrajhankatturrajarethinam <krganeshrajhan@gmail.com>
2021-08-29 16:57:48 +0800

    BubbleSort

---
+++ src/main/java/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package algorithms.sorting;
+
+import algorithms.sorting.helper.SortingHelper;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,0,5,95,4,-100,200,0};
+        bubbleSort(arr, arr.length);
+        SortingHelper.printArr(arr);
+    }
+
+    private static void bubbleSort(int[] arr, int length) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+}
