9980489589f44441fb5e40fc335739068a065545
ganeshrajhankatturrajarethinam <krganeshrajhan@gmail.com>
2021-08-31 10:32:05 +0800

    KnapsackProblem brute force

---
+++ src/main/java/algorithms/dynamic/programming/KnapsackProblem.java
@@ -0,0 +1,29 @@
+package algorithms.dynamic.programming;
+
+public class KnapsackProblem {
+
+    public static int knapsack(int profits[], int profitsLength, int weights[], int weightsLength, int capacity, int index) {
+
+        if(capacity < 0 || index == weightsLength || weights[index] > capacity) {
+            return 0;
+        }
+
+        int profit = profits[index] + knapsack(profits, profitsLength, weights, weightsLength, capacity - weights[index], index + 1);
+
+        int nextProfit = knapsack(profits, profitsLength, weights, weightsLength, capacity, index + 1);
+        return Math.max(profit, nextProfit);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int profit[] = {60, 100, 120};
+        int profitsLength = 3;
+        int weight[] = {10, 20, 30};
+        int weightsLength = 3;
+        int capacity = 50;
+        System.out.println(knapsack(profit, profitsLength, weight, weightsLength, capacity, 0));
+    }
+
+}
